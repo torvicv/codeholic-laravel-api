925b2f42ad3325d46299aa97caaa4c99a25128eb
victor <torvicv@gmail.com>
2024-05-11 18:17:58 +0200

    added login route

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/login', [AuthController::class, 'authenticate']);
+// Route::post('/login', [AuthController::class, 'authenticate']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
