8c1799c5d2d6cf6784573e6ff1f8973ee232b628
victor <torvicv@gmail.com>
2024-05-11 20:30:03 +0200

    csrf y HasApiTokens

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            'http://localhost:8000/api/register',
+            'http://localhost:8000/api/login',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
