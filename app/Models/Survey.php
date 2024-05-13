9917c71087762f3971654ac23d645ce8608a3980
victor <torvicv@gmail.com>
2024-05-13 12:25:21 +0200

    survey request, controller and routes.

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Survey extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'expire_date',
+        'status',
+        'slug',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,7 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('/survey', SurveyController::class);
 });
 
 // Route::post('/login', [AuthController::class, 'authenticate']);
